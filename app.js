22f931a9b22aebb9181624c460b0b163afc4eeef
yash <yash04577@gmail.com>
2022-12-18 18:56:07 +0530

    layout fix

---
+++ app.js
@@ -0,0 +1,23 @@
+go_home = () => {
+    window.scrollTo(300,0);
+}
+
+go_about = () => {
+    window.scrollTo(300, 570);
+}
+
+go_menu = () => {
+    window.scrollTo(300, 1100);
+}
+
+go_products = () => {
+    window.scrollTo(300, 1860);
+}
+
+go_review = () => {
+    window.scrollTo(300, 2460);
+}
+
+go_contact = () => {
+    window.scrollTo(300, 3000);
+}
